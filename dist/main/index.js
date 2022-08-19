14a20cec42442a5aee6fe3970c162378ef62fb52
sam bacha <sam@manifoldfinance.com>
2022-08-18 23:31:49 -0700

    fix(build): output

--- dist/main/index.js
+++ dist/main/index.js
@@ -3940,16 +3940,39 @@ exports["default"] = _default;
 
 /***/ }),
 
-/***/ 399:
-/***/ ((__unused_webpack_module, exports, __nccwpck_require__) => {
+/***/ 144:
+/***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
 
 "use strict";
 
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.IsPost = void 0;
-const core = __nccwpck_require__(186);
-const coreCommand = __nccwpck_require__(351);
-const exec = __nccwpck_require__(514);
+const core = __importStar(__nccwpck_require__(186));
+const coreCommand = __importStar(__nccwpck_require__(351));
+const exec = __importStar(__nccwpck_require__(514));
 exports.IsPost = Boolean(process.env.STATE_isPost);
 const name = core.getInput('name', { required: true });
 const extraPullNames = core.getInput('extraPullNames');
@@ -4174,7 +4197,7 @@ module.exports = require("util");
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module is referenced by other modules so it can't be inlined
-/******/ 	var __webpack_exports__ = __nccwpck_require__(399);
+/******/ 	var __webpack_exports__ = __nccwpck_require__(144);
 /******/ 	module.exports = __webpack_exports__;
 /******/ 	
 /******/ })()
